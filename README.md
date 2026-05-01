bd36b9fab92c7bc6bf946faa758de456191663b5
ninad <ninadhazarika@gmail.com>
2026-05-01 11:08:05 +0530

    Initial commit: LED control experiment

---
+++ README.md
@@ -0,0 +1,6 @@
+# ESP32 Experiments
+
+This repository contains my experiments with ESP32, sensors, and embedded systems.
+
+## Projects
+- LED Control
---
+++ led control/sketch_apr23b.ino
@@ -0,0 +1,23 @@
+#define ninad 2
+
+ 
+
+void setup() {
+
+  pinMode(ninad, OUTPUT); 
+
+}
+
+void loop() { 
+
+  
+
+  digitalWrite(ninad, HIGH);  
+
+  delay(1000);             
+
+  digitalWrite(ninad, LOW);  
+
+  delay(1000);         
+
+}
